fde522bf91ae64c69b9775b0fb7b4dd011abaf9f
mgopi6013 <mgopi2310@gmail.com>
2026-06-17 10:22:55 +0530

    Update del_one.c

--- del_one.c
+++ del_one.c
@@ -1,99 +1,89 @@
-//----------------Delete one node or record--------//
 #include"student.h"
-void del_one(struct st **ptr)
+int id=0;
+int main()
 {
-	char dop,dc,dname[20];
-	int drol,dnr;
-	struct st *temp,*prev;
-	printf("\n");
-	printf("\t\t+---------------------------------------+\t\t\n");
-	printf("\t\t|     CHOOSE THE OPTION TO DELETE       |\t\t\n");
-	printf("\t\t+---------------------------------------|\t\t\n");
-	printf("\t\t|   R/r  :  Enter roll.no to delete     |\t\t\n");
-	printf("\t\t|   N/n  :  Enter Name to delete        |\t\t\n");
-	printf("\t\t+---------------------------------------+\t\t\n");
-	printf("\n");
-	printf("\tEnter the choice : ");
-	scanf(" %c",&dop);
-	switch(dop|32)
+	struct st *hptr=0;
+	char ch,w;
+	load(&hptr);
+	if(hptr!=0)
 	{
-		case 'r': printf("Enter roll.no to delete : ");
-			  scanf("%d",&drol);
-			  temp=*ptr;
-			  while(temp!=0)
-			  {
-				  if(temp->roll==drol)
-				  {
-					  printf("\t\t+---------------+---------------+---------------+\n");
-					  printf("\t\t|     CONFIRM THE STUDENT RECORD TO DELETE      |\n");
-					  printf("\t\t+---------------+---------------+---------------+\n");
-					  printf("\t\t| %-13d | %-13s | %-13.2f |\n",temp->roll,temp->name,temp->mark);
-					  printf("\t\t+---------------+---------------+---------------+\n");
-					  printf("\n\t\tDO YOU WANT TO DELETE THIS STUDENT RECORD : ");
-					  scanf(" %c",&dc);
-					  if(dc=='y')
-					  {
-						  if(temp==*ptr)
-							  *ptr=temp->next;
-						  else
-							  prev->next=temp->next;
-						  free(temp);
-						  return;
-					  }
-					  else
-						  printf("\t\tDELETE OPERATION IS CANCELED\n");
-				  }
-				  else
-				  {
-					  prev=temp;
-					  temp=temp->next;
-				  }
-			  }
-			  if(temp==0)
-				  printf("\t\tTHERE IS NO STUDENT ID IS MATCHED\n");
+	struct st *last=hptr;
+	while(last->next!=0)
+		last=last->next;
+	id=last->roll;
+	}
+	while(1){
+	printf("\n");
+	printf("\t\t+-------------------------------------+\t\t\n");
+	printf("\t\t|        STUDENT RECORD MENU          |\t\t\n");
+	printf("\t\t+-------------------------------------+\t\t\n");
+	printf("\t\t|    A/a    : Add New Record          |\t\t\n");
+	printf("\t\t|    D/d    : Delete A Record         |\t\t\n");
+	printf("\t\t|    S/s    : Show the list           |\t\t\n");
+	printf("\t\t|    M/m    : Modify the record       |\t\t\n");
+	printf("\t\t|    V/v    : Save                    |\t\t\n");
+	printf("\t\t|    E/e    : Exit                    |\t\t\n");
+	printf("\t\t|    T/t    : Sort the list           |\t\t\n");
+	printf("\t\t|    L/l    : Delete All records      |\t\t\n");
+	printf("\t\t|    R/r    : Reverse the list        |\t\t\n");
+	printf("\t\t+-------------------------------------+\t\t\n\n");
+
+	printf("Choose the option in the menu : ");
+	scanf(" %c",&ch);
+	switch(ch|32)
+	{
+		case 'a': add_new(&hptr);
+			  printf("\nTHE NEW STUDENT DETAIL IS ADDED IN THE LIST\n");
+                          while(getchar() != '\n'); 
+                          getchar();                 
+			  system("clear");
 			  break;
-		case 'n': printf("Enter Name to delete : ");
-			  scanf("%s",dname);
-			  temp=*ptr;
-			  printf("\t\t+---------------+---------------+---------------+\n");
-			  printf("\t\t|     CONFIRM THE STUDENT RECORD TO DELETE      |\n");
-			  printf("\t\t+---------------+---------------+---------------+\n");
-			  while(temp!=0)
-			  {
-				  if(strcmp(temp->name, dname) == 0)
-				  {
-
-					  printf("\t\t| %-13d | %-13s | %-13.2f |\n",temp->roll,temp->name,temp->mark);
-				  }
-				  temp=temp->next;
-			  }
-			  printf("\t\t+---------------+---------------+---------------+\n");
-			  printf("\nENTER THE ROLL.NO TO DELETE THE PARTICULAR STUDENT RECORD : \n");
-			  scanf("%d",&dnr);
-			  temp=*ptr;
-			  while(temp!=0)
-			  {
-				  if(temp->roll==dnr)
-				  {
-					  if(temp==*ptr)
-						  *ptr=temp->next;
-					  else
-						  prev->next=temp->next;
-					  free(temp);
-					  return;
-				  }
-				  else
-				  {
-					  prev=temp;
-					  temp=temp->next;
-				  }
-			  }
-			  if(temp==0)
-				  printf("\n\t\tTHERE IS NO STUDENT ID IS MATCHED\n");	 
-
+		case 'd': del_one(&hptr);
+			  printf("\nTHIS PARTICULAR STUDENT DETAIL IS DELETED\n");
+                          while(getchar() != '\n'); 
+                          getchar();                 
+			  system("clear");
+			  break;
+		case 's': printf("\nTHESE IS STUDENTS LIST\n"); 
+                          show(hptr);
+			  printf("\nPress Enter to ENTER THE STUDENT RECORD MENU\n");
+                          while(getchar() != '\n'); 
+                          getchar();                 
+                          system("clear");
+			  break;
+		case 'm': modify(hptr);
+			  printf("\nSTUDENT DETAIL IS MODIFIED\n");
+                          while(getchar() != '\n'); 
+                          getchar();                 
+			  system("clear");
+			  break; 
+		case 'v': save(hptr);
+			  printf("\nALL THE STUDENT DETAILS ARE SAVED\n");
+                          while(getchar() != '\n'); 
+                          getchar();                 
+			  system("clear");
+			  break; 
+		case 'e': exit_std(hptr);
+			  printf("\nEXIT FROM THE STUDENT RECORD\n");
+			  system("clear");
+			  return 0;
+		case 't': sort(hptr);
+			  printf("\nTHE STUDENT RECORDS ARE SORTED\n");
+                          while(getchar() != '\n'); 
+                          getchar();                 
+			  system("clear");
+			  break;
+		case 'l': del_all(&hptr);
+			  printf("\nALL THE STUDENT RECORDS ARE DELETED\n");
+			  sleep(3);
+			  system("clear");
+			  break;
+		case 'r': reverse(hptr);
+			  printf("\nALL THE STUDENT RECORDS ARE REVERSED\n");
+                          while(getchar() != '\n'); 
+                          getchar();                 
+			  system("clear");
 			  break;
 	}
-
+	}
 }
-
-
